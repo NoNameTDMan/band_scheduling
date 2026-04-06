55581c6afda36c7d0090c5ab5acf1b4670d6279f
NoNameTDMan <takumi0729m2t8@gmail.com>
2026-04-06 23:04:57 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.set_page_config(page_title="固定枠作成ツール", page_icon=":material/music_note:")
 
-top_page = st.Page(page="contents/top_page.py", title="トップページ（仮）", icon=":material/home:")
-scheduling = st.Page(page="contents/band_practice_schedule.py", title="固定枠作成ツール", icon=":material/handyman:")
+top_page = st.Page(page="contents/top_page.py", title="How to Use", icon=":material/info:")
 make_csv = st.Page(page="contents/make_csv.py", title="CSVファイル作成ツール", icon=":material/csv:")
-pg = st.navigation([top_page, scheduling, make_csv])
+scheduling = st.Page(page="contents/band_practice_schedule.py", title="固定枠作成ツール", icon=":material/handyman:")
+pg = st.navigation([top_page, make_csv, scheduling])
 pg.run()
 
